f222f861a3600b71ba740b41249de0a247a9aa7d
Niranjan Ramadas <nrbramadas@gmail.com>
2013-04-18 13:35:10 -0700

    Fixed link

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Personalpage::Application.routes.draw do
   root to: "main#index"
   match "asteroids" => "static#asteroids"
   match "snake" => "static#snake"
+  match "tictactoe" => "static#tictactoe"
 end
